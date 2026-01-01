607053caa5a51b21ac48a544cf46943d9f1e2d32
Adrian Josan <josan.adrian11@gmail.com>
2026-01-01 18:22:56 +0200

    7. optimize

--- p7/rev_int.c
+++ p7/rev_int.c
@@ -2,48 +2,44 @@
 #include <stdlib.h>
 
 unsigned int pow_int(int a, int b) {
-    // if (b==0) return 1;
     int res = 1;
-    while (b>0) {
-        res = res * a;
-        b--;
-    }
+    while (b>0) { res = res * a; b--; }
     return res;
 }
 
 int reverse(int x){
-    printf("x=%d\n",x);
+    // printf("x=%d\n",x);
     if (x==0) return 0;
     int is_signed = (x<0);
-    printf("is_signed=%d\n",is_signed);
+    // printf("is_signed=%d\n",is_signed);
     unsigned int u_x = x;
     if (is_signed) u_x = -u_x;
 
     unsigned int res = 0;
     unsigned int div = u_x;
-    int digits = 0;
+    short digits = 0;
     do {
         div = div / 10;
         digits++;
     } while (div!=0);
-    printf("digits=%d\n\n", digits);
-    int* ints = malloc(digits * sizeof(int));
+    // printf("digits=%d\n\n", digits);
 
     for (int i = digits-1; i>=0; i--) {
-        printf("digit_i=%d\n",i);
-        if (((digits-(i+1))>=9) && ((u_x / pow_int(10,i)) % 10)>4) return 0;
-        int to_add = ((u_x / pow_int(10,i)) % 10) * pow_int(10,(digits-(i+1)));
+        // printf("digit_i=%d\n",i);
+        short pow_of_10 = (digits-(i+1));
+
+        if ((pow_of_10>9) || (pow_of_10==9 && ((u_x / pow_int(10,i)) % 10>4))) return 0;
+        int to_add = (u_x / pow_int(10,i)) % 10 * pow_int(10,pow_of_10);
         if (to_add > INT32_MAX) return 0;
-        if (!is_signed && (res + to_add)>INT32_MAX) {printf("x out of range MAX\n"); return 0; }
-        if (is_signed && (res + to_add)>=INT32_MAX) {printf("x out of range MIN\n"); return 0; }
         res += to_add;
-        // printf("pow_int(10,i) mod 10)=%d\n",);
-        // x -= (x % pow_int(10,i)) * (pow_int(10,digits-1));
-        printf("res=%d\n",res);
-        printf("\n");
+        if ((!is_signed && res>INT32_MAX) || (is_signed && res>=INT32_MAX)) {
+            // printf("x out of range MAX\n"); 
+            return 0; 
+        }
+        // printf("res=%d\n",res);
+        // printf("\n");
     }
     if (is_signed) return -res;
-
     return res;
 }
 
