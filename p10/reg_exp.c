4b9029fffda8714923aaeccbe63fe2217452780c
Adrian Josan <josan.adrian11@gmail.com>
2026-01-05 01:32:30 +0200

    10. optimize runtime&mem

--- p10/reg_exp.c
+++ p10/reg_exp.c
@@ -6,16 +6,10 @@
 // will parse first regex element
 
 bool isMatch(char* str, char* reg) {
-    printf("checking for str=%s, reg=%s\n",str,reg);
+    // printf("checking for str=%s, reg=%s\n",str,reg);
 
-    // printf("\n");
     int str_len = strlen(str);
     int reg_len = strlen(reg);
-
-    
-    // if (str_len==2 && str[0]=='b') {
-    //     printf("    again: checking for str=%s, reg=%s\n",str,reg);
-    // }
 
     if (str_len>0 && reg_len==0) {
         // printf("non-empty string and empty regex -> false\n");
@@ -48,17 +42,15 @@ bool isMatch(char* str, char* reg) {
         return false;
     }
 
+    bool use_wildcard_star = false;
     if (reg_len>=2 && reg[1] == '*') {                 // when star
         // printf("    when star\n");
 
         // optimization to skip repeating c* e.g. c*c*c*c* -> c*
-            // TODO: also optimize when sequence of star elements that contain at elas one .* :
-                // e.g. a*b*c*.*a*b* => .*
         if (reg_len>=4) {
             unsigned int add_to_reg = 0;
-            bool has_wildcard_star = false;
             if (reg[0]=='.') {
-                has_wildcard_star = true;
+                use_wildcard_star = true;
                 for (int i = 1; i < reg_len/2; i++) {
                     if (reg[i*2+1]=='*') {
                         add_to_reg+=2;
@@ -68,29 +60,20 @@ bool isMatch(char* str, char* reg) {
                 }
             } else {
                 for (int i = 1; i < reg_len/2; i++) {
-                    if (reg[i*2]=='.' && reg[i*2+1]=='*') has_wildcard_star = true;
-                    if ((reg[i*2]==reg[0] || has_wildcard_star) && reg[i*2+1]=='*') {
+                    if (reg[i*2]=='.' && reg[i*2+1]=='*') use_wildcard_star = true;
+                    if ((reg[i*2]==reg[0] || use_wildcard_star) && reg[i*2+1]=='*') {
                         add_to_reg+=2;
                     } else {
                         break;
                     }
                 }
             }
-            printf("        star->non-wildcard-> optimizing for %d of %c*; reg_len=%d\n",(add_to_reg),reg[0],reg_len);
+            // printf("        star->non-wildcard-> optimizing for %d of %c*; reg_len=%d\n",(add_to_reg),reg[0],reg_len);
             reg += add_to_reg;
-            if (has_wildcard_star) {
-                size_t reg_len = strlen(reg);
-                char* new_reg = malloc(sizeof(char)*(reg_len+1));
-                strcpy(new_reg, reg);
-                new_reg[reg_len]='\0';
-                reg = new_reg;
-                reg[0] = '.';
-                printf("            has_wildcard_star -> created new reg=%s\n",reg);
-            }
             reg_len = strlen(reg);
         }
 
-        if (reg[0]=='.') {                              // when wildcard
+        if (reg[0]=='.' || use_wildcard_star) {                              // when wildcard
             // printf("        when wildcard\n");
             if (reg_len==2) {
                 // printf("        .* is last regex elem\n");
@@ -105,10 +88,7 @@ bool isMatch(char* str, char* reg) {
 
         } else if (reg[0] != '.') {                     // non-wildcard
             // printf("        non-wildcard\n");
-
-
             if (reg_len==2 && str_len==1 && str[0] == reg[0]) return true;
-            // TODO: count until what index str matches c*, and then isMatch from largest str+i to smallest => more efficient
             int matching_chars = 0;
             for (int s_i = 0; s_i < str_len; s_i++) {
                 if (str[s_i]==reg[0]) {
@@ -159,9 +139,6 @@ bool isMatch(char* str, char* reg) {
     // printf("returning false\n\n");
     return false;
 }
-
-// for (r_i, r_i < reg_len, i++):
-    // if (regex[r_i+1] == )
 
 int main() {
     // char* str= "aaabc";  // match
