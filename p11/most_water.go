5f2b302de049780b3bd6fc917c59aeef7eaf581c
Adrian Josan <josan.adrian11@gmail.com>
2026-01-06 22:06:57 +0200

    11. optimal runtime

--- p11/most_water.go
+++ p11/most_water.go
@@ -1,26 +1,33 @@
 package main
 
-func maxArea(h []int) int {
-	var max_area int = 0
+import (
+	"fmt"
+)
+
+func maxArea(h []int) (max_area int) {
+	max_area = -1
 	var h_len int = len(h)
-	for i := 0; i < h_len-1; i++ {
-		if ((h_len-1)-i)*h[i] >= max_area {
-			for j := h_len - 1; j > i; j-- {
-				// if largest possible area is already smaller than current max area -> skip
-				if (j-i)*h[i] <= max_area {
-					break
-				}
-				if h[i] > h[j] && (j-i)*h[j] > max_area {
-					max_area = (j - i) * h[j]
-				} else if h[i] <= h[j] && (j-i)*h[i] > max_area {
-					max_area = (j - i) * h[i]
-				}
-			}
+	var i, j int = 0, h_len - 1
+	var current_area int
+
+	for {
+		if i == j {
+			break
+		}
+		if h[i] <= h[j] {
+			current_area = (j - i) * h[i]
+			i++
+		} else {
+			current_area = (j - i) * h[j]
+			j--
+		}
+		if current_area > max_area {
+			max_area = current_area
 		}
 	}
 	return max_area
 }
 
 func main() {
-	// fmt.Println(maxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7}))
+	fmt.Println(maxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7}))
 }
