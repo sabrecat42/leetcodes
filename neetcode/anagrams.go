f4d51d6973e01ec3fce30b37d371803cf826606b
Adrian Josan <josan.adrian11@gmail.com>
2026-01-23 23:36:21 +0100

    neet group anagrams - optimal

---
+++ neetcode/anagrams.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func groupAnagrams(strs []string) [][]string {
+	len_strs := len(strs)
+	lettersMap := make(map[[26]int][]string)
+	for i := 0; i < len_strs; i++ {
+		letterCountCard := [26]int{}
+		for _, char := range strs[i] {
+			letterCountCard[char-97]++
+		}
+		lettersMap[letterCountCard] = append(lettersMap[letterCountCard], strs[i])
+	}
+	finalList := [][]string{}
+	for _, sublist := range lettersMap {
+		finalList = append(finalList, sublist)
+	}
+	return finalList
+}
+
+func main() {
+	// r1 := 'b'
+	// fmt.Println(r1 - 97)
+	res := groupAnagrams([]string{""})
+	fmt.Println(res)
+}
